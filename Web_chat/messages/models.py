e2b7b78253d7f05c6c9374e1a75bc65bf54e226d
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-10-13 13:53:47 +0300

    created messages model

--- Web_chat/messages/models.py
+++ Web_chat/messages/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from Web_chat.users.models import CustomUser
+
+
+class CustomMessage(models.Model):
+    MESSAGE_MAX_LEN = 250
+
+    sender = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        on_update=models.CASCADE,
+        related_name='sent_messages'
+    )
+    recipient = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        on_update=models.CASCADE,
+        related_name='received_messages'
+    )
+    content = models.TextField(max_length=MESSAGE_MAX_LEN)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_seen = models.BooleanField(default=False)
+    is_deleted = models.BooleanField(default=False)
+
+    def mark_as_seen(self):
+        self.is_seen = True
+        self.save()
+
+    def mark_as_deleted(self):
+        self.is_deleted = True
+        self.save()
+
+    def __str__(self):
+        return f"From: {self.sender} | To: {self.recipient} | {self.content[:50]}"
