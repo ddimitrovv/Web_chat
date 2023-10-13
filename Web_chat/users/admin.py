05842d39a6aef169c60fc56b9f2d242b2a81ec6b
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-10-13 14:10:01 +0300

    registered user and user_profile to admin

--- Web_chat/users/admin.py
+++ Web_chat/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from Web_chat.users.models import CustomUser, UserProfile
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'is_active', 'is_staff', 'is_superuser']
+    search_fields = ['username']
+    search_help_text = 'Search for user by username'
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'gender', 'user']
+    search_fields = ['user__username']
+    search_help_text = 'Search for user profile by username'
