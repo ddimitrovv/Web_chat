ff6c06cff8bcaf100d1f6be4b5a4e937b5e98a5a
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-10-13 13:43:28 +0300

    added user model and profile

--- Web_chat/settings.py
+++ Web_chat/settings.py
@@ -111,7 +111,20 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+
+
+# Media files (images)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (
+        BASE_DIR / 'media/'
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.CustomUser'
